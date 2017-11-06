e79f30911b0090da75a2e36d61edbcbe3391b237
omguhh <yasmine.m.f@live.co.uk>
2017-11-06 21:16:49 +0400

    responsive navigation set up

---
+++ src/scripts/navigation.js
@@ -0,0 +1,10 @@
+/**
+ * Created by yasmi on 11/4/2017.
+ */
+
+var hamburgerMenu = document.getElementsByClassName('navigation__hamburger')[0];
+var menuItemsWrapper = document.getElementsByClassName('navigation__items')[0];
+
+hamburgerMenu.addEventListener('click', function(){
+    menuItemsWrapper.classList.toggle('navigation__items--active');
+});
